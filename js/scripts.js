906959989ee97526b4a3ac35835748dd643170a5
Lucian <lucian.miller185@gmail.com>
2021-05-26 09:49:15 -0700

    It will create for loop to count up by the number that the user has given, to another number that the user has given

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-  $("form#input").submit(function(event){
+  $("form#input").submit(function(event) {
+    const highestNumber = parseInt($("#limit").val());
+    console.log(highestNumber);
+    const countBy = parseInt($("#count").val());
+    console.log(countBy);
+
+    for (let index = countBy; index <= highestNumber; index += countBy) {
+      console.log(index);
+    }
     event.preventDefault();
   });
 });
